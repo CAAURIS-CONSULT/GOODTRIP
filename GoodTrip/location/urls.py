0dac63fce3a2448363e21637885c9c967363c266
ElenaPetrowski <gkmichael01@gmail.com>
2021-08-17 10:06:43 +0000

    17-08-2021 10h06

--- GoodTrip/location/urls.py
+++ GoodTrip/location/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from location.views import accueil, ajaxView
+from location.views import accueil, ajaxView, rechercheVoiture
 urlpatterns = [
     path("", accueil, name="acc_location"),
     path("marque",ajaxView,name="marque"),
+    path('recherche', rechercheVoiture, name = 'rechercheVoiture'),
 ]
--- GoodTrip/location/views.py
+++ GoodTrip/location/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from location.models import Marque, Modele, Vehicule, Category
 from django.http import HttpResponse, JsonResponse
+from django.db.models import Q
 # Create your views here.
 def accueil(request):
     template_name = 'location/accueil.html'
@@ -17,7 +18,26 @@ def accueil(request):
 def ajaxView(request):
     marque = request.GET.get('marque')
     idMarque = Marque.objects.filter(nom_marque=marque)[0].id
-    listModel = [Modele.objects.filter(marque=idMarque)[i].nom_modele for i in range(len(Modele.objects.filter(marque=idMarque)))]
-    
-    print(listModel)
+    listModel = [Modele.objects.filter(marque=idMarque)[i].nom_modele for i in range(len(Modele.objects.filter(marque=idMarque)))]    
     return JsonResponse({'models':listModel})
+
+def rechercheVoiture(request):
+        if request.method  == 'GET':
+            modele = request.GET.get('modeles')
+            marque = request.GET.get('marque')
+            category = request.GET.get('category')
+            marqueId = Marque.objects.filter(nom_marque = marque)
+            modeleId = Modele.objects.filter(nom_modele = modele)
+            categoryId = Category.objects.filter(nom_category = category)
+
+            params = Q(category=categoryId)
+            params.add(Q(marque=marqueId),Q.AND)
+            params.add(Q(modele=modeleId),Q.AND)
+
+            resultat = Vehicule.objects.filter(params)
+            context = {
+                'resultat':resultat,
+            }
+            return render(request, 'achat-vehicule-grille.html', context)
+        else:
+            return redirect('acc_location')
