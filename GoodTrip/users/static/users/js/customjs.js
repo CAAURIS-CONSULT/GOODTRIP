4b181e75fad910430745261d7f92f4e7a716f724
ElenaPetrowski <gkmichael01@gmail.com>
2021-08-24 16:01:18 +0000

    24-08-2021 16H01

--- GoodTrip/users/static/users/js/customjs.js
+++ GoodTrip/users/static/users/js/customjs.js
@@ -1,5 +1,20 @@
 
 var data = {};
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }   
+    return cookieValue;
+};
 
 $('.marque').click(function(){
    var marque = $('#marque').val();
@@ -55,5 +70,23 @@ function getModelsById(id){
         error:function(){
                 alert('nononn');
             }
+    });
+}
+function commander(user_id, vehicule_id, quantity){
+    $.ajax({
+        url: 'http://localhost:8000/location/commande',
+        method: 'GET',
+        data: {
+            'user_id':user_id,
+            'vehicule_id':vehicule_id,
+            'quantity':quantity,
+        },
+        dataType:'application/json',
+        success:function(response){
+            alert(response);
+        },
+        error:function(){
+            alert(response);
+        }
     });
 }
