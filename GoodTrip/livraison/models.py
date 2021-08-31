1ddce078021bf4d3c2e677242c1c25a4f57371b5
ElenaPetrowski <gkmichael01@gmail.com>
2021-08-31 14:26:11 +0000

    31-08-2021 14h26

--- GoodTrip/livraison/models.py
+++ GoodTrip/livraison/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Localisation(models.Model):
+    nom_commune     = models.CharField(max_length = 100)
+
+class Trajet(models.Model):
+    depart          = models.ForeignKey(Localisation, on_delete=models.CASCADE, related_name='depart')
+    arrivee         = models.ForeignKey(Localisation, on_delete=models.CASCADE, related_name='arrivee')
+    cout_du_trajet  = models.PositiveIntegerField(default=5)
+    distance_approx = models.PositiveIntegerField(default=5)
+
 class Livraison(models.Model):
-    pass
-    
+    proprietaire    = models.ForeignKey(User, on_delete=models.CASCADE)
+    trajet          = models.ForeignKey(Trajet, on_delete=models.CASCADE)
+    status          = models.CharField(max_length =20, default = 'En cours')
