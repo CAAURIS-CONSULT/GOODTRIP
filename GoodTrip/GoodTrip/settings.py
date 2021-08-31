f6af03356b22f5d60621cc65bb4758b5ba1d2276
ElenaPetrowski <gkmichael01@gmail.com>
2021-08-31 09:25:18 +0000

    debut de livraison

--- GoodTrip/GoodTrip/settings.py
+++ GoodTrip/GoodTrip/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'users',
+    'livraison',
     'location',
     'django.contrib.admin',
     'django.contrib.auth',
--- GoodTrip/livraison/urls.py
+++ GoodTrip/livraison/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from livraison.views import accueil
 
 urlpatterns = [
-    
+    path('', accueil, name='acc_livraison'),
 ]
